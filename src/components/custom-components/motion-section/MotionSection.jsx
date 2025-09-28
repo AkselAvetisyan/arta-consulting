d916990411b11c9150cd17aec76a95accaf68552
Aksel <avetisyanaksel>
2025-09-29 02:17:15 +0400

    fix blur issue on motion section

--- src/components/custom-components/motion-section/MotionSection.jsx
+++ src/components/custom-components/motion-section/MotionSection.jsx
@@ -1,11 +1,12 @@
 import { motion } from "framer-motion";
 
 function MotionSection({ className, children, transition = 1.5 }) {
+
     return (
         <motion.section
             className={className}
             initial={{ opacity: 0, filter: "blur(10px)" }}
-            animate={{ opacity: 1, filter: "0" }}
+            animate={{ opacity: 1, filter: "unset" }}
             transition={{ duration: transition }}
         >
             {children}
